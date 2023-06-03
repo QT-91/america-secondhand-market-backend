e858d767424b2127bf22bf4a6e1dc7a0cf9f06fd
Minseo Kim <68639278+star14ms@users.noreply.github.com>
2023-06-03 08:59:56 -0400

    Feat: Use MySQL

--- asm_backend/settings/base.py
+++ asm_backend/settings/base.py
@@ -86,8 +86,10 @@ WSGI_APPLICATION = "asm_backend.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        "ENGINE": "django.db.backends.mysql",
+        "OPTIONS": {
+            "read_default_file": "./my.cnf",
+        },
     }
 }
 
